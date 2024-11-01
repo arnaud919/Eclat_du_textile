e060e8ea5a1bccb833ced0c2aa53c0f123ffc687
Arnaud Colombe <arnaudcolombe9@gmail.com>
2024-11-01 15:15:03 +0100

    change

--- back-end/eclat_du_textile/src/EventListener/PasswordHasherListener.php
+++ back-end/eclat_du_textile/src/EventListener/PasswordHasherListener.php
@@ -1,37 +1,53 @@
 <?php
-
 namespace App\EventListener;
 
 use Doctrine\ORM\Event\PrePersistEventArgs;
 use Doctrine\ORM\Event\PreUpdateEventArgs;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Psr\Log\LoggerInterface;
 use App\Entity\User;
 
 class PasswordHasherListener
 {
-    private UserPasswordHasherInterface $hasher;
+    private UserPasswordHasherInterface $passwordHasher;
+    private LoggerInterface $logger;
 
-    public function __construct(UserPasswordHasherInterface $hasher)
+    public function __construct(UserPasswordHasherInterface $passwordHasher, LoggerInterface $logger)
     {
-        $this->hasher = $hasher;
+        $this->passwordHasher = $passwordHasher;
+        $this->logger = $logger;
     }
 
     public function prePersist(PrePersistEventArgs $args): void
     {
-        dump("prePersist called for PasswordHasherListener");
-        $this->hashPassword($args->getObject());
+        $this->hashPassword($args);
     }
-    
 
     public function preUpdate(PreUpdateEventArgs $args): void
     {
-        $this->hashPassword($args->getObject());
+        $this->hashPassword($args);
     }
 
-    private function hashPassword($entity): void
+    private function hashPassword($args): void
     {
-        if ($entity instanceof User && $entity->getPassword()) {
-            $entity->setPassword($this->hasher->hashPassword($entity, $entity->getPassword()));
+        $entity = $args->getObject();
+
+        if (!$entity instanceof User) {
+            return;
+        }
+
+        $this->logger->info("Mot de passe avant hachage : " . $entity->getPassword());
+
+        if ($entity->getPassword()) {
+            $hashedPassword = $this->passwordHasher->hashPassword($entity, $entity->getPassword());
+            $entity->setPassword($hashedPassword);
+
+            $this->logger->info("Mot de passe après hachage : " . $hashedPassword);
+
+            // Pour PreUpdate, informer Doctrine que le champ 'password' a changé
+            if ($args instanceof PreUpdateEventArgs) {
+                $args->getEntityChangeSet()['password'] = $hashedPassword;
+            }
         }
     }
 }
